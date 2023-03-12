0cca3ab450180f22a148bc115a9dec372ca00daf
l0f01b2 <lawrence.freeman@walmart.com>
2023-03-12 12:15:44 -0700

    Add support for key and shell for listen

--- src/discovery/node.py
+++ src/discovery/node.py
@@ -1,3 +1,5 @@
+import asyncio
+from discovery.protocol import Protocol
 import discovery.util as util
 
 class Node:
@@ -8,18 +10,36 @@ class Node:
     3.  Count
     4.  Regenerate Key
     """
-    def __init__(self, id=0, host="127.0.0.1", port=1024, nextAvailable=True):
+    def __init__(self, id=0, host="127.0.0.1", port=1024, path="./test", nextAvailable=True):
         print("Node::init")
         self.host = host
         self.port = util.findNextPort(port) if nextAvailable else port
-        # generate private/public key if needed
+        self.path = f"{path}/instance{id}"
+        self.get_public_and_private_keys()
+        print(f"privateKey: {self._privateKey}, publicKey: {self._publicKey}")
+        self.protocol_class = Protocol(self)
+
+    def get_public_and_private_keys(self):
+        self._privateKey, self._publicKey = util.get_keys(self.path)
+        if self._privateKey is None or self._publicKey is None:
+            self._privateKey, self._publicKey = util.create_keys(self.path)
+
+    def create_protocol(self):
+        return self.protocol_class
 
     def join(self, node=None):
-        if node == None:
-            print("Node::join None")
-        else:
+        if node is not None:
             print("Node::join existing...") 
 
+        # listen
+        print(f"Node::listen: host: {self.host}, port: {self.port}")
+        loop = asyncio.get_event_loop()
+        listen = loop.create_datagram_endpoint(self.create_protocol,
+                                               local_addr=(self.host, self.port))
+        transport, protocol = loop.run_until_complete(listen)
+        self.protocol_class.save_protocol_and_transport(protocol=protocol, transport=transport)
+            
+        
     def regenerateKey(self):
         print("Node::regenerateKey")
 
--- src/discovery/util.py
+++ src/discovery/util.py
@@ -1,4 +1,78 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import ec
+from typing import Final
+from cryptography.hazmat.primitives import hashes
+import os
+from cryptography.hazmat.primitives import serialization
+
+def create_keys(path):
+    privateKey, publicKey = generate_private_and_public_keys()
+    privateKeyFile = get_private_key_filename(path)
+    publicKeyFile = get_public_key_filename(path)
+    create_path_if_needed(path)
+    write_bytes_to_file(privateKey, privateKeyFile)
+    write_bytes_to_file(publicKey, publicKeyFile)
+    return (privateKey, publicKey) 
+
+def create_path_if_needed(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
 
 def findNextPort(port=1024):
     print(f"util::findNextPort")
     return port
+
+CURVE: Final = ec.SECP256K1()
+SIGNATURE_ALGORITHM: Final = ec.ECDSA(hashes.SHA256())
+
+def generate_private_and_public_keys():
+    curve = ec.SECP256K1()
+    privateKey = ec.generate_private_key(curve)
+    serializedPrivateKey = privateKey.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+    publicKey = privateKey.public_key()
+    serializedPublicKey = publicKey.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    return (serializedPrivateKey, serializedPublicKey)
+
+def get_keys(path):
+    if not os.path.exists(path):
+        return (None, None)
+    privateKeyFile = get_private_key_filename(path)
+    publicKeyFile = get_public_key_filename(path)
+    return (get_private_key_from_pem_file(privateKeyFile),
+            get_public_key_from_pem_file(publicKeyFile)) 
+
+def get_private_key_filename(path):
+    return f"{path}/node.private.key.pem"
+
+def get_private_key_from_pem_file(privateKeyFile):
+    privateKeyBytes = read_bytes_from_file(privateKeyFile)
+    return serialization.load_pem_private_key(
+        privateKeyBytes,
+        password=None,
+        backend=default_backend()
+    )
+
+def get_public_key_filename(path):
+    return f"{path}/node.public.key.pem"
+    
+def get_public_key_from_pem_file(publicKeyFile):
+    publicKeyBytes = read_bytes_from_file(publicKeyFile)
+    publicKey = serialization.load_pem_public_key(publicKeyBytes)
+    return publicKey.public_bytes(serialization.Encoding.X962,
+                                  serialization.PublicFormat.CompressedPoint).hex()
+
+def read_bytes_from_file(fileWithPath):
+    with open(f"{fileWithPath}", "rb") as byteFile:
+        return byteFile.read()
+
+def write_bytes_to_file(bytesToWrite, fileWithPath):
+    with open(f"{fileWithPath}", "wb") as byteFile:
+        byteFile.write(bytesToWrite)
